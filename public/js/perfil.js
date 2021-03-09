924e36b227ffa03a2ac2ca9cbf5f85a78b063ce1
pakinro7 <pakinro7rai@gmail.com>
2021-03-09 20:23:37 +0100

    HOLA xd

--- public/js/perfil.js
+++ public/js/perfil.js
@@ -205,13 +205,16 @@ $(document).ready(function() {
         }
     });
 
+    //-----------
     //Ajax o introducir datos en la base de datos
-    //Ajax o introducir datos en la base de datos
+    //-----------
+
+    //CAMBIAR NOMBRE
     function changeName(name) {
         $.ajax({
             type: "GET",
             url: "/changename",
-            data: { newName: name },
+            data: { new: name },
 
             // ------v-------use it as the callback function
             success: function(data) {
@@ -228,11 +231,12 @@ $(document).ready(function() {
         changeName($(this).siblings("input[type='text']").val());
     });
 
+    //CAMBIAR APELLIDO1
     function changeApellido1(name) {
         $.ajax({
             type: "GET",
             url: "/changeapellido1",
-            data: { newApellido1: name },
+            data: { new: name },
 
             // ------v-------use it as the callback function
             success: function(data) {
@@ -249,11 +253,12 @@ $(document).ready(function() {
         changeApellido1($(this).siblings("input[type='text']").val());
     });
 
+    //CAMBIAR APELLIDO2
     function changeApellido2(name) {
         $.ajax({
             type: "GET",
             url: "/changeapellido2",
-            data: { newApellido2: name },
+            data: { new: name },
 
             // ------v-------use it as the callback function
             success: function(data) {
@@ -270,11 +275,12 @@ $(document).ready(function() {
         changeApellido2($(this).siblings("input[type='text']").val());
     });
 
+    //CAMBIAR DNI
     function changeDni(name) {
         $.ajax({
             type: "GET",
             url: "/changedni",
-            data: { newDni: name },
+            data: { new: name },
 
             // ------v-------use it as the callback function
             success: function(data) {
@@ -291,11 +297,12 @@ $(document).ready(function() {
         changeDni($(this).siblings("input[type='text']").val());
     });
 
+    //CAMBIAR FECHA NACIMIENTO
     function changeFechaNacimiento(name) {
         $.ajax({
             type: "GET",
             url: "/changefechanacimiento",
-            data: { newFechaNacimiento: name },
+            data: { new: name },
 
             // ------v-------use it as the callback function
             success: function(data) {
@@ -310,6 +317,248 @@ $(document).ready(function() {
 
     $("#fechaNacimiento .mostrar button").on("click",function(){
         changeFechaNacimiento($(this).siblings("input[type='date']").val());
+    });
+
+    //CAMBIAR GENERO
+    function changeGenero(name) {
+        $.ajax({
+            type: "GET",
+            url: "/changegenero",
+            data: { new: name },
+
+            // ------v-------use it as the callback function
+            success: function(data) {
+                console.log(data);
+                window.location.reload();
+            },
+            error: function(request, error) {
+                console.log(request, error);
+            }
+        });        
+    }
+
+    $("#genero .mostrar button").on("click",function(){
+        changeGenero($(this).siblings("input[type='radio']:checked").val());
+    });
+
+    //CAMBIAR CALLE
+    function changeCalle(name) {
+        $.ajax({
+            type: "GET",
+            url: "/changecalle",
+            data: { new: name },
+
+            // ------v-------use it as the callback function
+            success: function(data) {
+                console.log(data);
+                window.location.reload();
+            },
+            error: function(request, error) {
+                console.log(request, error);
+            }
+        });        
+    }
+
+    $("#calle .mostrar button").on("click",function(){
+        changeCalle($(this).siblings("input[type='text']").val());
+    });
+
+    //CAMBIAR NUMERO DOMICILIO
+    function changeNumeroDireccion(name) {
+        $.ajax({
+            type: "GET",
+            url: "/changenumerodireccion",
+            data: { new: name },
+
+            // ------v-------use it as the callback function
+            success: function(data) {
+                console.log(data);
+                window.location.reload();
+            },
+            error: function(request, error) {
+                console.log(request, error);
+            }
+        });        
+    }
+
+    $("#numeroDireccion .mostrar button").on("click",function(){
+        changeNumeroDireccion($(this).siblings("input[type='number']").val());
+    });
+
+    //CAMBIAR ESCALERA/PISO/ETC
+    function changeEscaleraPiso(name) {
+        $.ajax({
+            type: "GET",
+            url: "/changeescalerapiso",
+            data: { new: name },
+
+            // ------v-------use it as the callback function
+            success: function(data) {
+                console.log(data);
+                window.location.reload();
+            },
+            error: function(request, error) {
+                console.log(request, error);
+            }
+        });        
+    }
+
+    $("#escaleraPiso .mostrar button").on("click",function(){
+        changeEscaleraPiso($(this).siblings("input[type='text']").val());
+    });
+
+    //CAMBIAR MUNICIPIO
+    function changeMunicipio(name) {
+        $.ajax({
+            type: "GET",
+            url: "/changemunicipio",
+            data: { new: name },
+
+            // ------v-------use it as the callback function
+            success: function(data) {
+                console.log(data);
+                window.location.reload();
+            },
+            error: function(request, error) {
+                console.log(request, error);
+            }
+        });        
+    }
+
+    $("#municipio .mostrar button").on("click",function(){
+        changeMunicipio($(this).siblings("input[type='text']").val());
+    });
+
+    //CAMBIAR PROVINCIA
+    function changeProvincia(name) {
+        $.ajax({
+            type: "GET",
+            url: "/changeprovincia",
+            data: { new: name },
+
+            // ------v-------use it as the callback function
+            success: function(data) {
+                console.log(data);
+                window.location.reload();
+            },
+            error: function(request, error) {
+                console.log(request, error);
+            }
+        });        
+    }
+
+    $("#provincia .mostrar button").on("click",function(){
+        changeProvincia($(this).siblings("input[type='text']").val());
+    });
+
+    //CAMBIAR CODIGO POSTAL
+    function changeCodigoPostal(name) {
+        $.ajax({
+            type: "GET",
+            url: "/changecodigopostal",
+            data: { new: name },
+
+            // ------v-------use it as the callback function
+            success: function(data) {
+                console.log(data);
+                window.location.reload();
+            },
+            error: function(request, error) {
+                console.log(request, error);
+            }
+        });        
+    }
+
+    $("#codigoPostal .mostrar button").on("click",function(){
+        changeCodigoPostal($(this).siblings("input[type='number']").val());
+    });
+
+    //CAMBIAR TELEFONO MOVIL
+    function changeTelefonoMovil(name) {
+        $.ajax({
+            type: "GET",
+            url: "/changetelefonomovil",
+            data: { new: name },
+
+            // ------v-------use it as the callback function
+            success: function(data) {
+                console.log(data);
+                window.location.reload();
+            },
+            error: function(request, error) {
+                console.log(request, error);
+            }
+        });        
+    }
+
+    $("#movil .mostrar button").on("click",function(){
+        changeTelefonoMovil($(this).siblings("input[type='number']").val());
+    });
+
+    //CAMBIAR TELEFONO FIJO
+    function changeTelefonoFijo(name) {
+        $.ajax({
+            type: "GET",
+            url: "/changetelefonofijo",
+            data: { new: name },
+
+            // ------v-------use it as the callback function
+            success: function(data) {
+                console.log(data);
+                window.location.reload();
+            },
+            error: function(request, error) {
+                console.log(request, error);
+            }
+        });        
+    }
+
+    $("#fijo .mostrar button").on("click",function(){
+        changeTelefonoFijo($(this).siblings("input[type='number']").val());
+    });
+
+    //CAMBIAR WEB PERSONAL
+    function changeWebPersonal(name) {
+        $.ajax({
+            type: "GET",
+            url: "/changewebpersonal",
+            data: { new: name },
+
+            // ------v-------use it as the callback function
+            success: function(data) {
+                console.log(data);
+                window.location.reload();
+            },
+            error: function(request, error) {
+                console.log(request, error);
+            }
+        });        
+    }
+
+    $("#webPersonal .mostrar button").on("click",function(){
+        changeWebPersonal($(this).siblings("input[type='text']").val());
+    });
+
+    //CAMBIAR NOMBRE JUGADOR
+    function changeNombreJugador(name) {
+        $.ajax({
+            type: "GET",
+            url: "/changenombrejugador",
+            data: { new: name },
+
+            // ------v-------use it as the callback function
+            success: function(data) {
+                console.log(data);
+                window.location.reload();
+            },
+            error: function(request, error) {
+                console.log(request, error);
+            }
+        });        
+    }
+
+    $("#playerName .mostrar button").on("click",function(){
+        changeNombreJugador($(this).siblings("input[type='text']").val());
     });
 
 });
